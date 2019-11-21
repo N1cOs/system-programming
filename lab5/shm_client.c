c1c2dfc6c7a9aed44e9c98bccd7209192701e4f4
Nick <nickkarmatskikh@gmail.com>
2019-11-22 01:42:59 +0300

    lab5/shm_client.c: remove including of `lib.h`

--- lab5/shm_client.c
+++ lab5/shm_client.c
@@ -1,11 +1,10 @@
-#include "lib.h"
 #include "client.h"
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
 int main(int argc, char *argv[]) {
     key_t key = ftok(PATH_NAME, PROJ_ID);
-    int shmid = shmget(key, sizeof(struct info), 0644 | IPC_CREAT);
+    int shmid = shmget(key, sizeof(struct info), 0644);
     if (shmid == -1) {
         fprintf(stderr, "%s: %s: %s\n", argv[0], "shmget", strerror(errno));
         return EXIT_FAILURE;
