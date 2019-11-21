193f33e6fb404c98974b87faef755ee600e44d12
Nick <nickkarmatskikh@gmail.com>
2019-11-22 01:44:09 +0300

    lab5/lib.h: add structs for queue ipc

--- lab5/lib.h
+++ lab5/lib.h
@@ -7,10 +7,12 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
-#define PATH_NAME "/tmp/server.shm"
+#define PATH_NAME "/tmp/lab5"
 #define PROJ_ID 123
 
 #define SHMAT_FAILED (void*) -1
+
+#define MSG_TYPE 1
 
 #ifndef _INFO_DEFINED_
 #define _INFO_DEFINED_
@@ -22,5 +24,21 @@ struct info {
     double load_1;
     double load_5;
     double load_15;
+};
+#endif
+
+#ifndef _MSG_BODY_DEFINED_
+#define _MSG_BODY_DEFINED_
+struct msg_body {
+    int qid;
+    struct info info;
+};
+#endif
+
+#ifndef _MSG_DEFINED_
+#define _MSG_DEFINED_
+struct message {
+    long type;
+    struct msg_body body;
 };
 #endif
