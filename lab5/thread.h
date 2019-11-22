6a2c9d8263a955c17c5bde39b9cee7a57f2ec48b
Nick <nickkarmatskikh@gmail.com>
2019-11-23 02:01:00 +0300

    lab5/thread.h: add header file for threads tasks

---
+++ lab5/thread.h
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/types.h>
+
+#define ALPHABET_LEN 26
+ 
+extern char alphabet[ALPHABET_LEN];
+
+void invert_case();
+void reverse();
+void print_alphabet();
