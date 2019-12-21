a95a872dbfe0f91c63e7edc27565c0762741627d
Nick <nickkarmatskikh@gmail.com>
2019-12-21 03:44:07 +0300

    lab6/lib.h: add common lib

---
+++ lab6/lib.h
@@ -0,0 +1,15 @@
+#include <time.h>
+#include <stdio.h>
+#include <errno.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define DEFAULT_PORT 8000
+#define DEFAULT_HOST "localhost"
